05a3e1cedcaaf99cc0c010771299d49c9b423a1f
carrymaniac <838537276@qq.com>
2020-07-24 15:48:34 +0800

    [Story 1] [henry] feature: add should_return_car_ticket_when_park_given_car

---
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -0,0 +1,30 @@
+package com.oocl.cultivation;
+
+/**
+ * @ProjectName: parking-lot
+ * @Package: com.oocl.cultivation
+ * @ClassName: ParkingBoy
+ * @Author: carrymaniac
+ * @Description:
+ * @Date: 2020/7/24 3:47 下午
+ * @Version:
+ */
+public class ParkingBoy {
+    public ParkingLot getParkingLot() {
+        return parkingLot;
+    }
+
+    public void setParkingLot(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+    private ParkingLot parkingLot;
+
+    public ParkingBoy(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+    public CarTicket park(Car car) {
+        return null;
+    }
+}
---
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyTest.java
@@ -0,0 +1,31 @@
+package com.oocl.cultivation.test;
+
+import com.oocl.cultivation.Car;
+import com.oocl.cultivation.CarTicket;
+import com.oocl.cultivation.ParkingBoy;
+import com.oocl.cultivation.ParkingLot;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * @ProjectName: parking-lot
+ * @Package: com.oocl.cultivation.test
+ * @ClassName: ParkingBoyTest
+ * @Author: carrymaniac
+ * @Description:
+ * @Date: 2020/7/24 3:44 下午
+ * @Version:
+ */
+public class ParkingBoyTest {
+    @Test
+    void should_return_car_ticket_when_park_given_car() {
+        //given
+        Car car = new Car();
+        ParkingBoy boy = new ParkingBoy(new ParkingLot());
+        //when
+        CarTicket carTicket = boy.park(car);
+        //then
+        assertNotNull(carTicket);
+    }
+}
