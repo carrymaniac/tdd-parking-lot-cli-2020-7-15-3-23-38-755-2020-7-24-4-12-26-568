96a1795539c9adcaa0b11cbedf8513d26e969973
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-27 17:41:15 +0800

    [story 5] [henry] test: implement should_park_4_car_when_park_given_two_parking_boy_and_two_parking_lot_and_4_car

--- src/main/java/com/oocl/cultivation/ParkingLotServiceManager.java
+++ src/main/java/com/oocl/cultivation/ParkingLotServiceManager.java
@@ -1,19 +1,27 @@
 package com.oocl.cultivation;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class ParkingLotServiceManager implements ParkingBoy{
+    List<Parkable> parkables = new ArrayList<>();
     public ParkingLotServiceManager(Parkable... parkables) {
-
+        this.getParkingLots().addAll(Arrays.asList(parkables));
     }
 
     @Override
     public List<Parkable> getParkingLots() {
-        return null;
+        return this.parkables;
     }
 
     @Override
     public ParkResult park(Car car) {
-        return null;
+        return parkables.stream()
+                .filter(parkable -> parkable.getRemainingPosition() > 0)
+                .findFirst()
+                .orElse(parkables.get(0))
+                .park(car);
     }
 }
