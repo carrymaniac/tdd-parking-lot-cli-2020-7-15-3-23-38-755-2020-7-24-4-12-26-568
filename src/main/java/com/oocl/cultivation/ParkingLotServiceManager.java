b4a5fbe9ed126e2ae79b238f774373a29fffb875
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-27 17:26:35 +0800

    [story 5] [henry] feature: add should_park_4_car_when_park_given_two_parking_boy_and_two_parking_lot_and_4_car test

---
+++ src/main/java/com/oocl/cultivation/ParkingLotServiceManager.java
@@ -0,0 +1,19 @@
+package com.oocl.cultivation;
+
+import java.util.List;
+
+public class ParkingLotServiceManager implements ParkingBoy{
+    public ParkingLotServiceManager(Parkable... parkables) {
+
+    }
+
+    @Override
+    public List<ParkingLot> getParkingLots() {
+        return null;
+    }
+
+    @Override
+    public ParkResult park(Car car) {
+        return null;
+    }
+}
--- src/test/java/com/oocl/cultivation/test/ParkingLotServiceManagerTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingLotServiceManagerTest.java
@@ -1,11 +1,31 @@
 package com.oocl.cultivation.test;
 
+import com.oocl.cultivation.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingLotServiceManagerTest {
 
     @Test
     void should_park_4_car_when_park_given_two_parking_boy_and_two_parking_lot_and_4_car() {
-//        ParkingLotServiceManager parkingLotServiceManager = new
+        ParkingLot parkingLotA = new ParkingLot(1);
+        ParkingLot parkingLotB = new ParkingLot(1);
+        ParkingBoy parkingBoy = new NormalParkingBoy(Arrays.asList(new ParkingLot(1)));
+        ParkingBoy smartParkingBoy = new SmartParkingBoy(Arrays.asList(new ParkingLot(1),new ParkingLot(1)));
+        ParkingLotServiceManager parkingLotServiceManager = new ParkingLotServiceManager(parkingLotA,parkingLotB,parkingBoy,smartParkingBoy);
+        List<ParkResult> parkResults = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            parkingLotServiceManager.park(new Car());
+        }
+        assertEquals(4,parkResults.size());
+        parkResults.stream().forEach(parkResult -> {
+            assertNotNull(parkResult.getCarTicket());
+        });
     }
 }
